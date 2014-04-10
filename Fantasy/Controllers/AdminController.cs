16786b9272553f237b7bf1f8b13ae5a2e8f5d03c
lana90 <lhajro1@etf.unsa.ba>
2014-04-10 02:06:42 +0200

    Nova infrastruktura, modifikacije baze - privremeni projekat

---
+++ Fantasy/Controllers/AdminController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Fantasy.Models;
+
+namespace Fantasy.Controllers
+{
+    public class AdminController : Controller
+    {
+        private fantasyEntities db = new fantasyEntities();
+
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            return View(db.admin.ToList());
+        }
+
+        //
+        // GET: /Admin/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            admin admin = db.admin.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        //
+        // GET: /Admin/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Admin/Create
+
+        [HttpPost]
+        public ActionResult Create(admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.admin.Add(admin);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(admin);
+        }
+
+        //
+        // GET: /Admin/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            admin admin = db.admin.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        //
+        // POST: /Admin/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(admin).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(admin);
+        }
+
+        //
+        // GET: /Admin/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            admin admin = db.admin.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        //
+        // POST: /Admin/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            admin admin = db.admin.Find(id);
+            db.admin.Remove(admin);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FantasyServices/AdminService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Web;
+using System.Web.Security;
+using Newtonsoft.Json;
+using System.Web.Script.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using System.Data.Entity.Validation;
+
+namespace FantasyServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "AdminService" in both code and config file together.
+    public class AdminService : IAdminService
+    {
+        
+
+
+        public bool IsValid(string user, string password)
+        {
+            fantasyEntities db = new fantasyEntities();
+
+            using (db)
+            {
+                if (db.admin.Where(a => a.email.Equals(user) && a.password.Equals(password)).FirstOrDefault() != null)
+                {
+                    return true;
+                }
+                else return false;
+            }
+
+
+
+        }
+
+
+        public bool CreateAdmin(string obj)
+        {
+            string provjera="";
+            
+            try
+            {
+                fantasyEntities db = new fantasyEntities();
+
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(admin));
+                MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(obj));
+                admin a = db.admin.Add((admin)ser.ReadObject(ms));
+                db.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    provjera = eve.Entry.Entity.GetType().Name + " " + eve.Entry.State;
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        provjera= ve.PropertyName+" "+ve.ErrorMessage;
+                    }
+                }
+               
+            }
+
+            if(provjera.Equals("")) return true;
+            else return false;
+
+          
+        }
+    }
+}
+    
+
